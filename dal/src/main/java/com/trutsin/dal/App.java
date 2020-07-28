dc687d7803f5bc70f7f9efe690a0ed036b8a3255
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-07-28 18:09:54 +0300

    Create multi-module and dal projects

---
+++ dal/src/main/java/com/trutsin/dal/App.java
@@ -0,0 +1,13 @@
+package com.trutsin.dal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
