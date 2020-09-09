4e81cb71e85bdd6810b43c94102048d38422da80
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-09-09 11:16:20 +0300

    Creat config for web and initializer for application. Add dependency in pom(web)

---
+++ web/src/main/java/WebApplicationInitializer.java
@@ -0,0 +1,31 @@
+import com.trutsin.config.RootConfig;
+import com.trutsin.config.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] {characterEncodingFilter};
+    }
+}
---
+++ web/src/main/java/com/trutsin/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.trutsin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.trutsin.controller")
+@EnableWebMvc
+public class WebConfig {
+    @Bean
+    ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
