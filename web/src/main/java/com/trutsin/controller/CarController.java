9a0a1052aa57f9761f3996390d1817555b275dbc
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-09-22 17:18:53 +0300

    Dlete unnecessary class Film

--- web/src/main/java/com/trutsin/controller/CarController.java
+++ web/src/main/java/com/trutsin/controller/CarController.java
@@ -1,7 +1,6 @@
 package com.trutsin.controller;
 
 import com.trutsin.dal.entity.Car;
-import com.trutsin.model.Film;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
